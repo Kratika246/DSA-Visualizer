74e13a256dccc09af477199ab377c49239493e77
Kratika246 <guptakratika246@gmail.com>
2026-03-06 23:20:49 +0530

    resolved type errors in factorial.ts

--- algorithms/recursion/basic/factorial.ts
+++ algorithms/recursion/basic/factorial.ts
@@ -3,10 +3,11 @@ import { RecursionStep } from "@/types/Step";
 const Factorial = (args: number[]) => {
   const n = args[0];
   let steps: RecursionStep[] = [];
+  
   let cs: RecursionStep['callStack'] = [];
 
   const fact = (num: number): number => {
-    // push call
+    // 1. Push Initial Call
     cs.push({ fn: "factorial", args: [num] });
     steps.push({
       type: "recursion",
@@ -16,9 +17,10 @@ const Factorial = (args: number[]) => {
       activeCallIndex: cs.length - 1,
     });
 
-    // base case
+    // 2. Base Case
     if (num === 0 || num === 1) {
-      cs[cs.length - 1] = {...cs[cs.length-1], returned:1 };
+      // Update current frame with return value
+      cs[cs.length - 1] = { ...cs[cs.length - 1], returned: 1 };
 
       steps.push({
         type: "recursion",
@@ -27,39 +29,45 @@ const Factorial = (args: number[]) => {
         callStack: [...cs],
         activeCallIndex: cs.length - 1,
       });
+
+      // Pop from stack after visualizing return
       cs.pop();
       steps.push({
         type: "recursion",
         treeNodes: [],
         activeNodeId: 0,
         callStack: [...cs],
-        activeCallIndex: cs.length - 1,
+        // FIX: Ensuring number type (-1) instead of undefined
+        activeCallIndex: cs.length > 0 ? cs.length - 1 : -1,
       });
 
       return 1;
     }
 
-    // recursive case
+    // 3. Recursive Case
     const res = num * fact(num - 1);
 
-    // return step
-    cs[cs.length - 1] = {...cs[cs.length-1], returned: res};
+    // 4. Return Step Visualization
+    cs[cs.length - 1] = { ...cs[cs.length - 1], returned: res };
 
-      steps.push({
-        type: "recursion",
-        treeNodes: [],
-        activeNodeId: 0,
-        callStack: [...cs],
-        activeCallIndex: cs.length - 1,
-      });
-      cs.pop();
-      steps.push({
-        type: "recursion",
-        treeNodes: [],
-        activeNodeId: 0,
-        callStack: [...cs],
-        activeCallIndex: cs.length? cs.length - 1: undefined,
-      });
+    steps.push({
+      type: "recursion",
+      treeNodes: [],
+      activeNodeId: 0,
+      callStack: [...cs],
+      activeCallIndex: cs.length - 1,
+    });
+
+    // Pop the stack frame
+    cs.pop();
+    steps.push({
+      type: "recursion",
+      treeNodes: [],
+      activeNodeId: 0,
+      callStack: [...cs],
+      // FIX: Ensuring number type (-1) instead of undefined
+      activeCallIndex: cs.length > 0 ? cs.length - 1 : -1,
+    });
 
     return res;
   };
